23d2e8d0f92b9d8ad25dbc3617330d6659f63039
juan carlos concha <jconchah9@youremail.com>
2023-05-02 22:23:53 -0400

    return user data and token session

---
+++ README.md
@@ -0,0 +1,20 @@
+
+Para ejecutar el script es necesario instalar las dependencias
+
+```
+npm install
+```
+
+Luego podemos ejecutar el script con
+
+```
+npm run start 
+```
+
+Recuerda cambiar las variables que estan en index.js
+```javascript
+const user = {
+    email: "cambiar_email",
+    password: "cambiar_password"
+}
+```
--- index.js
+++ index.js
@@ -1,38 +1,46 @@
    
 const user = {
-    email: 'CAMBIAR_EMAIL',
-    password: "CAMBIAR_PASS"
+    email: "cambiar_email",
+    password: "cambiar_password"
 }
 
 const getUserInfoFromJumbo = async function(user){
-       const puppeteer = require('puppeteer');
-       const browser = await puppeteer.launch({ headless: false, executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome', devtools: true });
-       const page = await browser.newPage()
-       const url_supermarket = 'https://www.jumbo.cl/'
+    try {
+        const puppeteer = require('puppeteer');
+        const browser = await puppeteer.launch({ headless: 'new', executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' });
+        const page = await browser.newPage()
+        const url_supermarket = 'https://www.jumbo.cl/'
+        let user_data = {}
 
-       await page.goto(url_supermarket)
+        await page.goto(url_supermarket)
+        await page.setViewport({ width: 1440, height: 796 })
 
-       await page.setViewport({ width: 1440, height: 796 })
-       await page.waitForSelector('.new-header-wrapper > .new-header-container > .new-header-right-content > .new-header-wrap-links > .primary-btn')
-   
-       await page.click('button[aria-label="Ingresa"')
+        await page.waitForSelector('.new-header-wrapper > .new-header-container > .new-header-right-content > .new-header-wrap-links > .primary-btn')
+        await page.click('button[aria-label="Ingresa"')
+        await page.type('input[autocomplete="username"', user.email);
+        await page.type('input[name="Clave"]', user.password);
+        await page.click('.new-modal-content > .new-modal-container > .modal-actions > .primary-btn > .btn-span-enter')
 
+        const textRegex = /(json)/
 
-       await page.type('input[autocomplete="username"', user.email);
-       await page.type('input[name="Clave"]', user.password);
-       await page.click('.new-modal-content > .new-modal-container > .modal-actions > .primary-btn > .btn-span-enter')
-       const textRegex = /(json)/
+        page.on('response', async (response) => {
+            const headers = response.headers();
 
-       page.on('response', async (response) => {
-           const headers = response.headers();
+            const contentType = headers['content-type'];
+            if (textRegex.test(contentType) && /validate/.test(response.url())) {
+                const { token, user } = await response.json()
+                user_data.token = token
+                user_data.user = user
+            }
+        });
+        await page.waitForSelector('.close-session')
+        await browser.close();
 
-           const contentType = headers['content-type'];
-           if (textRegex.test(contentType) && /profile/.test(response.url())) {
-               console.log(response.url() + " data : " + (await response.json() ));
-           }
-       });
-       await page.waitForSelector('.close-session')
-       await browser.close();
+        console.log("%j", user_data);
+    } catch (error) {
+        console.log('Error al hacer login ', error);
+    }
+
    }
 
 getUserInfoFromJumbo(user)
